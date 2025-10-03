9e949b758771ff57b17999e5d54454a0fefb1a02
Sean H <DjPigPigstar@gmail.com>
2025-10-03 10:56:54 -0700

    Added multi param capabilities to Mixins

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -92,21 +92,40 @@ public class BaseLoggerMixinsTests
         Assert.AreEqual((LogLevel.Information, "test"), logger.LoggedMessages[0]);
     }
     #endregion
-
+    #region With Params : Valid
     [TestMethod]
-    public void Error_WithData_LogsMessage()
+    public void AllLevels_WithData_LogsMessage()
     {
         // Arrange
         TestLogger logger = new TestLogger();
 
         // Act
-        //logger.Error("Message {0}", 42);
+        logger.Error("Message {0}", 1);
+        logger.Warning("Message {0}", 2);
+        logger.Information("Message {0}", 3);
+        logger.Debug("Message {0}", 4);
+        //multiple methods Test
+        logger.Information("This Is a Multi param test {0} {0} {1} {2}", 4, "Who am i?", "Just another object?");
 
         // Assert
-        Assert.AreEqual(1, logger.LoggedMessages.Count);
+        Assert.AreEqual(5, logger.LoggedMessages.Count);
+
         Assert.AreEqual(LogLevel.Error, logger.LoggedMessages[0].LogLevel);
-        Assert.AreEqual("Message 42", logger.LoggedMessages[0].Message);
+        Assert.AreEqual("Message 1", logger.LoggedMessages[0].Message);
+
+        Assert.AreEqual(LogLevel.Warning, logger.LoggedMessages[1].LogLevel);
+        Assert.AreEqual("Message 2", logger.LoggedMessages[1].Message);
+
+        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[2].LogLevel);
+        Assert.AreEqual("Message 3", logger.LoggedMessages[2].Message);
+
+        Assert.AreEqual(LogLevel.Debug, logger.LoggedMessages[3].LogLevel);
+        Assert.AreEqual("Message 4", logger.LoggedMessages[3].Message);
+
+        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[4].LogLevel);
+        Assert.AreEqual("This Is a Multi param test 4 4 Who am i? Just another object?", logger.LoggedMessages[4].Message);
     }
+    #endregion
 }
 
 public class TestLogger : BaseLogger
--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -4,29 +4,29 @@ namespace Logger;
 
 public static class BaseLoggerMixins
 {
-    public static void Error(this BaseLogger logger, string message)
+    public static void Error(this BaseLogger logger, string message, params object[] arguments)
     {
         if (logger == null) 
             throw new ArgumentNullException();
 
-        logger.Log(LogLevel.Error, message);
+        logger.Log(LogLevel.Error, (arguments == null || arguments.Length == 0) ? message : string.Format(message,arguments));
     }
-    public static void Warning(this BaseLogger logger, string message)
+    public static void Warning(this BaseLogger logger, string message, params object[] arguments)
     {
         if (logger == null) throw new ArgumentNullException();
 
-        logger.Log(LogLevel.Warning, message);
+        logger.Log(LogLevel.Warning, (arguments == null || arguments.Length == 0) ? message : string.Format(message, arguments));
     }
-    public static void Information(this BaseLogger logger, string message)
+    public static void Information(this BaseLogger logger, string message, params object[] arguments)
     {
         if (logger == null) throw new ArgumentNullException();
 
-        logger.Log(LogLevel.Information, message);
+        logger.Log(LogLevel.Information, (arguments == null || arguments.Length == 0) ? message : string.Format(message, arguments));
     }
-    public static void Debug(this BaseLogger logger, string message)
+    public static void Debug(this BaseLogger logger, string message, params object[] arguments)
     {
         if (logger == null) throw new ArgumentNullException();
 
-        logger.Log(LogLevel.Debug, message);
+        logger.Log(LogLevel.Debug, (arguments == null || arguments.Length == 0) ? message : string.Format(message, arguments));
     }
 }
