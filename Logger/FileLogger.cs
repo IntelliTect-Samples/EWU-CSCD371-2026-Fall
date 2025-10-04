9b6ee41ba6347c520d604dddfd06725757dba2d4
Sean H <DjPigPigstar@gmail.com>
2025-10-04 01:14:46 -0700

    Switched out AppendAllText for AppendAllLines

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -9,7 +9,7 @@ public class FileLogger : BaseLogger
     public string? Path { get; set; }
     public override void Log(LogLevel logLevel, string message)
     {
-        File.AppendAllText(Path, $"{DateTime.Now} {this.Name} {logLevel} {message}{Environment.NewLine}");
+        File.AppendAllLines(Path, new[] { $"{DateTime.Now} {this.Name} {logLevel} {message}" });
     }
 
     public new static BaseLogger? Create(string className, string? filePath = null)
