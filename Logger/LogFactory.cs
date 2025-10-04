2cae96c21dcbe009f177efdc7f109a8f4f8e7179
Sean H <DjPigPigstar@gmail.com>
2025-10-04 00:34:17 -0700

    Revert "Simplification in CreateLogger"
    This reverts commit 2ea5ac01d857179336883f3efbd8a18774463652.

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -18,7 +18,9 @@ public class LogFactory
     where T : BaseLogger
     {
         MethodInfo method = typeof(T).GetMethod("Create", BindingFlags.Static | BindingFlags.Public);
+        if (method == null)
+            throw new InvalidOperationException($"{typeof(T).Name} must have a public static Create method.");
 
-        return (T?)method?.Invoke(null, [className, FilePath]);
+        return (T?)method.Invoke(null, [className, FilePath]);
     }
 }
