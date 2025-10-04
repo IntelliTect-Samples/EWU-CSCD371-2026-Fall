46e3297c2fe694842e0f1ed86bedb491ff73e3b0
Sean H <DjPigPigstar@gmail.com>
2025-10-03 17:46:07 -0700

    Adjusted code formatting

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -6,27 +6,26 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger logger, string message, params object[] arguments)
     {
-        if (logger == null) 
-            throw new ArgumentNullException();
+        if (logger == null) throw new ArgumentNullException();
 
-        logger.Log(LogLevel.Error, (arguments == null || arguments.Length == 0) ? message : string.Format(message,arguments));
+        logger.Log(LogLevel.Error, arguments?.Length == 0 ? message : string.Format(message,arguments));
     }
     public static void Warning(this BaseLogger logger, string message, params object[] arguments)
     {
         if (logger == null) throw new ArgumentNullException();
 
-        logger.Log(LogLevel.Warning, (arguments == null || arguments.Length == 0) ? message : string.Format(message, arguments));
+        logger.Log(LogLevel.Warning, arguments?.Length == 0 ? message : string.Format(message, arguments));
     }
     public static void Information(this BaseLogger logger, string message, params object[] arguments)
     {
         if (logger == null) throw new ArgumentNullException();
 
-        logger.Log(LogLevel.Information, (arguments == null || arguments.Length == 0) ? message : string.Format(message, arguments));
+        logger.Log(LogLevel.Information, arguments?.Length == 0 ? message : string.Format(message, arguments));
     }
     public static void Debug(this BaseLogger logger, string message, params object[] arguments)
     {
         if (logger == null) throw new ArgumentNullException();
 
-        logger.Log(LogLevel.Debug, (arguments == null || arguments.Length == 0) ? message : string.Format(message, arguments));
+        logger.Log(LogLevel.Debug, arguments?.Length == 0 ? message : string.Format(message, arguments));
     }
 }
