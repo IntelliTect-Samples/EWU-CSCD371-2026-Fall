913aa477a7206a5251f024eaa577fbff3af792e9
Nico Duvekot <nduvekot7@gmail.com>
2025-10-06 23:50:10 -0700

    test commit

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -2,6 +2,8 @@
 
 public abstract class BaseLogger
 {
+    // req1: The BaseLogger class needs an auto property to store class name
+    public string ClassName { get; set; } = string.Empty;
     public abstract void Log(LogLevel logLevel, string message);
 }
 
