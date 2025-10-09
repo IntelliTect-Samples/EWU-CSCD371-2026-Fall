3d3e389b4e4809814134c20a13f1d8f2799e6b4a
KorbinWeiler <korbinweiler@gmail.com>
2025-10-09 12:21:18 -0700

    Updated Testing

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -9,80 +9,109 @@ public class BaseLoggerMixinsTests
 {
     #region Extention Methods : Exception
     [TestMethod]
-    public void LogType_WithNullLogger_ThrowsException()
+    public void Error_WithNullLogger_ThrowsException()
     {
         // Arrange
         FileLogger fileLogger = null!;
         // Act
         // Assert
         Assert.ThrowsExactly<ArgumentNullException>(() => fileLogger.Error("Message"));
+    }
+
+    [TestMethod]
+    public void Debug_WithNullLogger_ThrowsException()
+    {
+        // Arrange
+        FileLogger fileLogger = null!;
+        // Act
+        // Assert
         Assert.ThrowsExactly<ArgumentNullException>(() => fileLogger.Debug("Message"));
+    }
+
+    [TestMethod]
+    public void Warning_WithNullLogger_ThrowsException()
+    {
+        // Arrange
+        FileLogger fileLogger = null!;
+        // Act
+        // Assert
         Assert.ThrowsExactly<ArgumentNullException>(() => fileLogger.Warning("Message"));
+    }
+
+    [TestMethod]
+    public void Information_WithNullLogger_ThrowsException()
+    {
+        // Arrange
+        FileLogger fileLogger = null!;
+        // Act
+        // Assert
         Assert.ThrowsExactly<ArgumentNullException>(() => fileLogger.Information("Message"));
     }
     #endregion
+
     #region Extention Methods : Valid
     [TestMethod]
-    public void AllLevels_WithNoData_LogsMessage()
+    public void Debug_WithData_LogsMessage()
+    {
+        // Arrange
+        TestLogger logger = new TestLogger();
+
+        // Act
+        logger.Debug("Message");
+
+        // Assert
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
+
+        Assert.AreEqual(LogLevel.Debug, logger.LoggedMessages[0].LogLevel);
+        Assert.AreEqual("Message", logger.LoggedMessages[0].Message);
+    }
+
+    [TestMethod]
+    public void Error_WithData_LogsMessage()
     {
         // Arrange
         TestLogger logger = new TestLogger();
 
         // Act
         logger.Error("Message");
-        logger.Warning("Message");
-        logger.Information("Message");
-        logger.Debug("Message");
 
         // Assert
-        Assert.AreEqual(4, logger.LoggedMessages.Count);
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
 
         Assert.AreEqual(LogLevel.Error, logger.LoggedMessages[0].LogLevel);
         Assert.AreEqual("Message", logger.LoggedMessages[0].Message);
-
-        Assert.AreEqual(LogLevel.Warning, logger.LoggedMessages[1].LogLevel);
-        Assert.AreEqual("Message", logger.LoggedMessages[1].Message);
-
-        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[2].LogLevel);
-        Assert.AreEqual("Message", logger.LoggedMessages[2].Message);
-
-        Assert.AreEqual(LogLevel.Debug, logger.LoggedMessages[3].LogLevel);
-        Assert.AreEqual("Message", logger.LoggedMessages[3].Message);
-
     }
-    #endregion
-    #region With Params : Valid
+
     [TestMethod]
-    public void AllLevels_WithData_LogsMessage()
+    public void Warning_WithData_LogsMessage()
     {
         // Arrange
         TestLogger logger = new TestLogger();
 
         // Act
-        logger.Error("Message {0}", 1);
-        logger.Warning("Message {0}", 2);
-        logger.Information("Message {0}", 3);
-        logger.Debug("Message {0}", 4);
-        //multiple methods Test
-        logger.Information("This Is a Multi param test {0} {0} {1} {2}", 4, "Who am i?", "Just another object?");
+        logger.Warning("Message");
 
         // Assert
-        Assert.AreEqual(5, logger.LoggedMessages.Count);
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
 
-        Assert.AreEqual(LogLevel.Error, logger.LoggedMessages[0].LogLevel);
-        Assert.AreEqual("Message 1", logger.LoggedMessages[0].Message);
+        Assert.AreEqual(LogLevel.Warning, logger.LoggedMessages[0].LogLevel);
+        Assert.AreEqual("Message", logger.LoggedMessages[0].Message);
+    }
 
-        Assert.AreEqual(LogLevel.Warning, logger.LoggedMessages[1].LogLevel);
-        Assert.AreEqual("Message 2", logger.LoggedMessages[1].Message);
+    [TestMethod]
+    public void Information_WithData_LogsMessage()
+    {
+        // Arrange
+        TestLogger logger = new TestLogger();
 
-        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[2].LogLevel);
-        Assert.AreEqual("Message 3", logger.LoggedMessages[2].Message);
+        // Act
+        logger.Information("Message");
 
-        Assert.AreEqual(LogLevel.Debug, logger.LoggedMessages[3].LogLevel);
-        Assert.AreEqual("Message 4", logger.LoggedMessages[3].Message);
+        // Assert
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
 
-        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[4].LogLevel);
-        Assert.AreEqual("This Is a Multi param test 4 4 Who am i? Just another object?", logger.LoggedMessages[4].Message);
+        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[0].LogLevel);
+        Assert.AreEqual("Message", logger.LoggedMessages[0].Message);
     }
     #endregion
 }
