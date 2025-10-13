9db9b437cb8775cefa07490cf00cc4d5ef830784
Nico Duvekot <nduvekot7@gmail.com>
2025-10-13 15:09:40 -0700

    Update JesterTests
    added 3 tests:
    
    1. ensuring with valid params a Jester Instance is made
    
    2. Ensuring with valid JokeService, passing a null for Output throws error
    
    3. Ensuring with valid OutputService, passing a null for JokeService throws error

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,8 +1,42 @@
+using Moq;
+using System;
 using Xunit;
 
 namespace CanHazFunny.Tests;
 
 public class JesterTests
 {
+    [Fact]
+    public void JesterConstructor_ValidParameters_CreatesJesterInstance()
+    {
+        // Arrange
+        Mock<IJokeService> jokeServiceMock = new();
+        Mock<IOutputService> outputServiceMock = new();
 
+        // Act
+        Jester jester = new(outputServiceMock.Object, jokeServiceMock.Object);
+
+        // Assert
+        Assert.NotNull(jester);
+    }
+
+    [Fact]
+    public void JesterConstructor_OutputServiceIsNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        Mock<IJokeService> jokeServiceMock = new();
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Jester(null!, jokeServiceMock.Object));
+    }
+
+    [Fact]
+    public void JesterConstructor_JokeServiceIsNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        Mock<IOutputService> outputServiceMock = new();
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Jester(outputServiceMock.Object, null!));
+    }
 }
