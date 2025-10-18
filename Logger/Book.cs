359a35f51ab6d86d2393a3f990a46994bbc2932f
Alex <108487553+Al3xramirez@users.noreply.github.com>
2025-10-18 16:54:35 -0700

    Added classes Book, Employee, Student

---
+++ Logger/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace Logger;
+
+// Change Book from a record to a class, since records cannot inherit from non-record classes.
+// Implement the required properties and constructor.
+public class Book : EntityBase
+{
+    public string Title { get; init; }
+    public string Author { get; init; }
+
+    
+    public override string Name
+    {
+        get => $"{Title} by {Author}";
+        set => throw new InvalidOperationException();
+    }
+}
