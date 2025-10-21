2c9dbdb42cd3a98968570d35d7f7a247a81d8d9c
Alex <108487553+Al3xramirez@users.noreply.github.com>
2025-10-20 20:32:55 -0700

    Changed constructor style

--- Logger/PersonBase.cs
+++ Logger/PersonBase.cs
@@ -1,10 +1,16 @@
 ﻿using Logger;
 using System;
 namespace Logger;
-public abstract record class PersonBase(Guid id, FullName fullName) : EntityBase
+public abstract record class PersonBase: EntityBase
 {
-    public new Guid Id { get; init; } = id;
-    public FullName FullName { get; init; } = fullName;
+    public new Guid Id { get; init; }
+    public FullName FullName { get; init; }
+
+    protected PersonBase(Guid id, FullName fullName)
+    {
+        Id = id;
+        FullName = fullName;
+    }
     public override string Name => FullName.Middle is null
         ? $"{FullName.First} {FullName.Last}"
         : $"{FullName.First} {FullName.Middle} {FullName.Last}";
