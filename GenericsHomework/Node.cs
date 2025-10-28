30157fdbc80de4f06999695d39ce92d7b2246c3d
Nico Duvekot <nduvekot7@gmail.com>
2025-10-27 21:25:30 -0700

    Project

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -3,7 +3,7 @@
 public class Node<T>
 {
     public T Value { get; }
-    public Node<T>? Next { get; private set; }
+    public Node<T> Next { get; private set; }
 
     public Node(T value)
     {
@@ -13,11 +13,55 @@ public class Node<T>
 
     public Node<T> Append(T value)
     {
-        var newNode = new Node<T>(value);
-        newNode.Next = this.Next;
+        if (Exists(value))
+        {
+            throw new InvalidOperationException($"Value '{value}' already exists in the list.");
+        }
+
+        Node<T> newNode = new(value)
+        {
+            Next = this.Next
+        };
         this.Next = newNode;
         return newNode;
     }
+
+    public bool Exists(T value)
+    {
+        Node<T> current = this;
+        do
+        {
+            if (Equals(current.Value, value))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        while (current != this);
+
+        return false;
+    }
+
+    public void Clear()
+    {
+        Node<T> current = this.Next;
+
+        if (ReferenceEquals(current, this))
+        {
+            return;
+        }
+
+        while (!ReferenceEquals(current.Next, this))
+        {
+            Node<T> next = current.Next;
+            current.Next = current; // Close the loop on the removed node
+            current = next;
+        }
+
+        current.Next = current;
+        this.Next = this;
+    }
+
 
     public override string ToString()
     {
@@ -25,3 +69,19 @@ public class Node<T>
     }
 }
 
+/*
+ * Garbage Colection Notes:
+ * As nodes are removed from the circular linked list using the Clear method, 
+ * their Next references are set to point to themselves. 
+ * This breaks the circular reference, 
+ * allowing the garbage collector to reclaim the memory used by these nodes 
+ * if there are no other references to them in the program.
+ * 
+ * Garbage Collection as I've understood it, as it pertains to this topic: 
+ * If an outside source still holds a reference to a node that has been "removed"
+ * it should be safer.
+ * but a removed node "pointing to itself" does not count,
+ * and therefore it could be collected.
+ * 
+ * See: https://essentialcsharp.com/runtime#garbage-collection
+ */
