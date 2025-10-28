eee830319ff949adb907b772db85b89332eabae0
Nico Duvekot <nduvekot7@gmail.com>
2025-10-27 20:07:42 -0700

    update: trying to switch to MS Test structure

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -2,6 +2,26 @@
 
 public class Node<T>
 {
+    public T Value { get; }
+    public Node<T>? Next { get; private set; }
 
+    public Node(T value)
+    {
+        Value = value;
+        Next = this;
+    }
+
+    public Node<T> Append(T value)
+    {
+        var newNode = new Node<T>(value);
+        newNode.Next = this.Next;
+        this.Next = newNode;
+        return newNode;
+    }
+
+    public override string ToString()
+    {
+        return Value?.ToString() ?? "null";
+    }
 }
 
