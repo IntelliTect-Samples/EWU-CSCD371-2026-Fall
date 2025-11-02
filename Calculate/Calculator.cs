6a6ed4f35b1af118beb67428cc98ea49f26ce0a7
Sean H <DjPigPigstar@gmail.com>
2025-11-01 17:48:25 -0700

    Initial Commit

---
+++ Calculate/Calculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate;
+
+public class Calculator
+{
+    public static IReadOnlyDictionary<char, Func<int,int,int>> MathematicalOperations = new Dictionary<char, Func<int, int, int>>()
+    {{ '+', Add }, { '-', Subtract }, { '*', Multiply }, { '/', Divide }};
+
+    public static int Add(int a, int b)
+    {
+        return checked(a + b);
+    }
+    public static int Subtract(int a, int b)
+    {
+        return checked(a - b);
+    }
+    public static int Multiply(int a, int b)
+    {
+        return checked(a * b);
+    }
+    public static int Divide(int a, int b)
+    {
+        if (b == 0) throw new DivideByZeroException($"Param {nameof(b)} was 0!");
+
+        return checked(a / b);
+    }
+
+    private static bool TryGetOperation(string input, out char operation)
+    {
+        operation = '_';
+        foreach (char iOperation in MathematicalOperations.Keys)
+        {
+            if (input.Contains($" {iOperation} "))
+            {
+                operation = iOperation;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool TryCalculate(string input, out int value)
+    {
+        value = 0;
+        if (!TryGetOperation(input, out char operation)) return false;
+
+        string[] parts = input.Split(operation);
+
+        if (parts.Length != 2) return false;
+
+        if (!int.TryParse(parts[0].Trim(), out int lhs)) return false;
+
+        if(!int.TryParse(parts[1].Trim(), out int rhs)) return false;
+
+        try
+        {
+            value = MathematicalOperations[operation](lhs, rhs);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            if(ex is DivideByZeroException || ex is OverflowException)
+                return false;
+            throw;
+        }
+    }
+
+}
---
+++ Calculate/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public Action<string> WriteLine { get; init; }
+    public Func<string?> ReadLine { get; init; }
+
+    public Program() {
+        WriteLine = Console.WriteLine;
+        ReadLine = Console.ReadLine;
+    }
+
+    public static int Main()
+    {
+        
+
+        return 0;
+    }
+}
