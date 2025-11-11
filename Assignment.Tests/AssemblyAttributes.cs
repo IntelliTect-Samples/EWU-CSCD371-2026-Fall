9a1797ff713230904d0428f8f0fd479d0a3b8099
Chloe Stanton <156254816+c-stanton@users.noreply.github.com>
2025-11-10 16:25:50 -0800

    Update AssemblyAttributes.cs

--- Assignment.Tests/AssemblyAttributes.cs
+++ Assignment.Tests/AssemblyAttributes.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 
 [assembly: Parallelize(Scope = ExecutionScope.ClassLevel)]
-
+namespace Assignment.Tests;
 [TestClass]
 public class PlaceholderTest
 {
     [TestMethod]
-    public void Placeholder_UsingLinqCount()
+    public void PlaceholderUsingLinqCount()
     {
         // Arrange
         var items = new List<int> { 1, 2, 3};
         // Act & Assert
-        Assert.AreEqual(3, items.Count(), "The list count check failed.");
+        Assert.AreEqual(3, items.Count, "The list count check failed.");
     }
 }
