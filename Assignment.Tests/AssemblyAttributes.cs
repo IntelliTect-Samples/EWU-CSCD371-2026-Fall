52ea8896a0206a0e214ff664f1717b6e387113ea
Sean H <DjPigPigstar@gmail.com>
2025-11-20 17:18:30 -0800

    Removed PlaceholderTest

--- Assignment.Tests/AssemblyAttributes.cs
+++ Assignment.Tests/AssemblyAttributes.cs
@@ -4,15 +4,3 @@ using System.Linq;
 
 [assembly: Parallelize(Scope = ExecutionScope.ClassLevel)]
 namespace Assignment.Tests;
-[TestClass]
-public class PlaceholderTest
-{
-    [TestMethod]
-    public void PlaceholderUsingLinqCount()
-    {
-        // Arrange
-        var items = new List<int> { 1, 2, 3};
-        // Act & Assert
-        Assert.HasCount(3, items, "The list count check failed.");
-    }
-}
